2d297694305a7432ac749d447ded479616b5cf0a
Chandra Shekhar <55743187+shekhar1190@users.noreply.github.com>
2024-01-15 18:29:39 +0530

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+           string result = Print(i);
+           Console.WriteLine(result);
+        }
+        Console.ReadLine();
+    }
+
+    public static string Print(int i)
+    {
+        if (i % 5 == 0 && i % 3 == 0) //15 Fizzbuzz
+        {
+            return "FizzBuzz";
+        }
+        else if(i % 3 == 0 && i % 5 != 0) //9 Fizz
+        {
+            return "Fizz";
+        }
+        else if (i % 5 == 0 && i % 3 != 0) //Buzz
+        {
+            return "Buzz";
+        }
+        else
+        {
+            return i.ToString(); //2,4
+        }
+    }
+}
+
+
---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,46 @@
+using Xunit;
+
+namespace TestProject
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(3, "Fizz")]
+        [InlineData(9, "Fizz")]
+        [InlineData(12, "Fizz")]
+        [InlineData(18, "Fizz")]
+        public void Print_Fizz_WhenDivideBy_3_Only(int input, string expected)
+        {
+            //Act
+            var res = Program.Print(input);
+            //Assert
+            Assert.Equal(res, expected);
+        }
+
+        [Theory]
+        [InlineData(5, "Buzz")]
+        [InlineData(10, "Buzz")]
+        [InlineData(20, "Buzz")]
+        [InlineData(25, "Buzz")]
+        public void Print_Fizz_WhenDivideBy_5_Only(int input, string expected)
+        {
+            //Act
+            var res = Program.Print(input);
+            //Assert
+            Assert.Equal(res, expected);
+        }
+
+        [Theory]
+        [InlineData(15, "FizzBuzz")]
+        [InlineData(30, "FizzBuzz")]
+        [InlineData(60, "FizzBuzz")]
+        [InlineData(90, "FizzBuzz")]
+        public void Print_Fizz_WhenDivideBy_5_AND_3(int input, string expected)
+        {
+            //Act
+            var res = Program.Print(input);
+            //Assert
+            Assert.Equal(res, expected);
+        }
+    }
+}
